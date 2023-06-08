812d8fd27a7ee2268b65cde9542d8587d8973c51
YunHeeW <helvetaw@gmail.com>
2023-06-08 18:15:35 +0900

    feat&design: 등록 기능, 기본 css 적용

--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -1,10 +1,59 @@
 import { CenteredOverlayForm } from "./CenteredOverlayForm";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import { useRecoilState } from "recoil";
+import { groupNameState } from "../state/groupName";
+import { useState } from "react";
 
 export const CreateGroup = () => {
+  const [groupName, setGroupName] = useRecoilState(groupNameState);
+  const [validated, setValidated] = useState(false);
+  const [validatedGroupName, setValidatedGroupName] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+
+    if (form.checkValidity()) {
+      setValidatedGroupName(true);
+    } else {
+      e.stopPropagation();
+      setValidatedGroupName(false);
+    }
+    setValidated(true);
+  };
+
   return (
     <div>
-      CreateGroup
-      <CenteredOverlayForm />
+      <h1>더치페이</h1>
+      <Container>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Row>
+            <h2>그룹 이름</h2>
+          </Row>
+          <Row>
+            <Form.Group controlId="validationGroupName">
+              <Form.Control
+                type="text"
+                required
+                placeholder="ex) 치킨먹기"
+                onChange={(e) => {
+                  setGroupName(e.target.value);
+                }}
+              />
+              <Form.Control.Feedback
+                type="invalid"
+                data-valid={validatedGroupName}
+              >
+                이름을 입력하세요
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Button type="submit">저장</Button>
+          </Row>
+        </Form>
+      </Container>
+      {/*<CenteredOverlayForm />*/}
     </div>
   );
 };
--- src/components/CreateGroup.spec.jsx
+++ src/components/CreateGroup.spec.jsx
@@ -1,12 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { CreateGroup } from "./CreateGroup";
 import userEvent from "@testing-library/user-event";
+import { RecoilRoot } from "recoil";
 const renderComponent = () => {
-  render(<CreateGroup />);
+  render(
+    <RecoilRoot>
+      <CreateGroup />
+    </RecoilRoot>
+  );
 
   const input = screen.getByPlaceholderText("치킨먹기");
   const saveButton = screen.getByText("저장");
-  const errorMessage = screen.queryByText("이름 입력하세요");
+  const errorMessage = screen.queryByText("이름을 입력하세요");
 
   return {
     input,
@@ -24,15 +29,13 @@ describe("그룹 생성 페이지", () => {
   test("그룹 이름 없이 저장 버튼 클릭 시", async () => {
     const { errorMessage, saveButton } = renderComponent();
     await userEvent.click(saveButton);
-
-    expect(errorMessage).not.toBeNull();
+    expect(errorMessage).toHaveAttribute("data-valid", "false");
   });
 
   test("그룹 이름 입력 후 저장 버튼 클릭 시(저장 성공)", async () => {
     const { input, saveButton, errorMessage } = renderComponent();
     await userEvent.type(input, "예시 이름");
     await userEvent.click(saveButton);
-
-    expect(errorMessage).toBeNull();
+    expect(errorMessage).toHaveAttribute("data-valid", "true");
   });
 });
